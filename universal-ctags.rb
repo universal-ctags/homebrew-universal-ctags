44c657145bc19eee9f167db6df85d3e2ff579510
KazuakiM <kazuaki_mabuchi_to_go@hotmail.co.jp>
2020-07-07 23:29:51 +0900

    docutils at optional, not install libtool at build, and update Travis setting.

--- universal-ctags.rb
+++ universal-ctags.rb
@@ -6,7 +6,8 @@ class UniversalCtags < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
-  depends_on "docutils" => :recommended
+  #depends_on "libtool" => :build
+  depends_on "docutils" => :optional
   depends_on "jansson" => :optional
   depends_on "libyaml" => :optional
   conflicts_with "ctags", :because => "this formula installs the same executable as the ctags formula"
